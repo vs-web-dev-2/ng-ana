b30a79e10d8f9a91577e6079c7e7137fc9a59e84
anauzubi <ana.uribe@softtek.com>
2021-06-22 10:27:25 +0200

    modulos y componebtes

--- src/app/pie/pie.component.ts
+++ src/app/pie/pie.component.ts
@@ -3,16 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pie',
   templateUrl: './pie.component.html',
-  styleUrls: ['./pie.component.css']
+  styleUrls: ['./pie.component.css'],
 })
 export class PieComponent implements OnInit {
-
-  public autor: string = "Ana Uribe";
-  public year: string = '2021';
+  public autor: string = 'Alberto Basalo';
+  // public year: string = '2021';
+  // public year: number = 2021;
+  public year: Date = new Date();
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void { }
 }
